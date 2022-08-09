db70c801739f0f5e9b0d0ac8219f0f74be7b3fa5
ido.lee <idonggeon917@gmail.com>
2022-08-09 23:09:30 +0900

    ds1_0.0.7

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const output = {
     home: (req,res) => {
         res.render("home/index");
@@ -11,9 +12,31 @@ const output = {
     
 };
 
+
+const users = {
+    id: ["sirius", "cello", "donggeon"],
+    psword: ["1234", "1234", "123456"],
+};
+
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+         psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
     },
 };
 
