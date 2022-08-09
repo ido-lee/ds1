a4b84da871bb00ab1251c703df13c624159089e9
ido.lee <idonggeon917@gmail.com>
2022-08-10 03:07:03 +0900

    ds1_models구현_0.0.9

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,8 +1,8 @@
 "use strict";
 
+const User = require("../../models/User");
 // const { response } = require("../../../app");
 
-const UserStorage = require("../../models/UserStorage")
 
 const output = {
     home: (req,res) => {
@@ -19,22 +19,8 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-          psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-              response.success = true;
-              return res.json(response);
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
