6023ff37922a95ce5ae1de6200b067b44ad68089
ido.lee <idonggeon917@gmail.com>
2022-08-13 04:24:58 +0900

    ds1 로그인 화면 꾸미기

--- app/app.js
+++ app/app.js
@@ -12,9 +12,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`)); // `${__dirname}` : 작성파일의 경로(app.js)     login ejs 에서 사용
 app.use(bodyParser.json());
+
 // url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use("/", home); //use -> 미드 웨어를 등록
 
