32564cc40bbb2e2bb1f17895c568ee657583de5e
ido.lee <idonggeon917@gmail.com>
2022-08-24 02:50:20 +0900

    fs모둘을 통한 회원가입 기능 구현 pt2

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,8 +16,9 @@ class UserStorage {
         return userInfo
     }
 
-    static getUsers(...fields) {
-        // const users = this.#users;
+    static #getUsers (data, isALL, fields) {
+        const users = JSON.parse(data);
+        if (isALL) return users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -25,6 +26,16 @@ class UserStorage {
             return newUsers;
         }, {});
         return newUsers;
+    }
+
+    static getUsers(isALL, ...fields) {
+        // const users = this.#users;
+        return fs
+        .readFile("./src/databases/users.json") 
+        .then((data) => {
+          return this.#getUsers(data, isALL, fields);
+        })
+         .catch(console.error);        
     }
 
     static getUserInfo(id) {
@@ -37,14 +48,18 @@ class UserStorage {
        
     }
 
-    static save(userInfo) {
-        // const users = this.#users;
+    static async save(userInfo) {
+        const users = await this.getUsers(true)
+        if (users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디 입니다.";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return { success: true};
+         // 데이터 추가
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return { success: true };
     }
-
 };
 
 module.exports = UserStorage
